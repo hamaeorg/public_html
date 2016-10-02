c2ad75249d80830fcdf6cccd28d55ab52547202d
Hannes Mäeorg <Hannes Meorg>
2016-10-02 16:23:01 +0300

    taimer ilusamaks

--- script.js
+++ script.js
@@ -1,6 +1,10 @@
 window.onload = function () {
     var intervalID = setInterval(function(){
-        document.getElementById("counter").innerHTML = getCounterValue();
+        var res = getCounterValue().split(":"); 
+        document.getElementById("day").innerHTML = res[0];
+        document.getElementById("hour").innerHTML = res[1];
+        document.getElementById("minute").innerHTML = res[2];
+        document.getElementById("second").innerHTML = res[3];
     }, 1000)
 };
 
@@ -16,5 +20,13 @@ function getCounterValue(){
     var days = (counter - hours)/ 24;
 
 
-    return days + ':' + hours + ':' + mins + ':' + secs;
+    return days + ':' + format(hours) + ':' + format(mins) + ':' + format(secs);
 }
+
+function format(number) {
+    number = number.toString();
+    if (number.length == 1) {
+        number = '0'  + number;
+    }
+    return number;
+ }
